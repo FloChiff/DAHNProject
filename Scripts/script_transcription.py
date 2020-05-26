18334fae64916aac2751f5e0799398cf9dd6f047
FloChiff <floriane.chiffoleau@hotmail.fr>
2020-05-26 12:12:57 +0200

    Modification of the transcription script

--- Scripts/script_transcription.py
+++ Scripts/script_transcription.py
@@ -1,23 +1,25 @@
-#Ligne de commande pour l'exécution du script : python nom_du_script dossier_d'entrée dossier_de_sortie chemin_vers_le_modèle
+# -*- UTF-8 -*-
+
+"""
+- author: Floriane Chiffoleau
+- date: May 2020
+- description: Image transcription using kraken and a transcription model 
+(it is recommended to have previously created a kraken environment and to activate it to run the command)
+- input: images
+- output: transcribed texts
+"""
 
 import os
-#Module pour travailler avec les fonctionnalités du système d'exploitation
 import subprocess
-#Module pour lancer de nouveaux processus
 import sys
-#Module pour communiquer avec le terminal
+
 images = sys.argv[1]
-#Création d'une variable pour stocker le chemin vers les images. Elle sera appelé comme deuxième argument dans le terminal.
 file = os.listdir(images)
-#Création d'une variable contenant une liste des éléments contenus dans le répertoire défini par la variable images (les images)
 texts = sys.argv[2]
-#Création d'une variable pour stocker le chemin vers le répertoire où seront créés les textes. Elle sera appelé comme troisième argument dans le terminal.
+
 for img in file:
     complete_path = f"{images}/{img}"
-    #Création d'une variable qui stocke le chemin vers les images à l'aide des variables précédemment déclarées
     output = f"{texts}/{img.replace('.JPG', '.txt')}"
-    #Création d'une variable qui stocke le chemin vers les fichiers de sortie
     subprocess.run(["kraken", "-i", complete_path, output, "binarize", "segment", "ocr", "--model", sys.argv[3]])
-    #Commande pour exécuter la transcription, avec les arguments présents entre crochets
-    #À l'intérieur des crochets, on retrouve tous les éléments nécessaires à la transcription d'une image avec kraken
-    #Soit kraken, le module input, le chemin vers les images à transcrire, le chemin de sortie, les commandes à exécuter pour la transcription, le module model et le chemin vers le modèle, appelé comme quatrième argument dans le terminal.
+    # The subprocess command allows the reproduction of the command line for a kraken transcription but with variables.  
+    # Those variables are used to call the content of folders instead of trancribing one image at a time.  
