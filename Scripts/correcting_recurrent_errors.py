718097fcd0939838ccbbdb659171857f0165fcd7
FloChiff <floriane.chiffoleau@hotmail.fr>
2020-07-20 15:53:07 +0200

    Add the 'correcting_recurrent_errors' script

---
+++ Scripts/correcting_recurrent_errors.py
@@ -0,0 +1,26 @@
+import sys
+from bs4 import BeautifulSoup
+
+with open(sys.argv[1], 'r') as xml_file:
+    print("reading from "+sys.argv[1])
+    tree = xml_file.read()
+tree = tree.replace('<p rend="indent"><closer>', '<closer>')
+tree = tree.replace('<p rend="indent"><salute rend="indent">', '<salute rend="indent">')
+#Deletion of the extra tags made because of the "text_tagging" script
+
+soup = BeautifulSoup(tree, 'xml')
+
+n = 1
+for pb in soup.find_all("pb"):
+    pb.attrs["n"] = n
+    n += 1
+    #Filling the @n attribute by numbering the pages
+
+    for lb in soup.find_all("lb"):
+        if pb.find_previous_sibling() == lb:
+            lb.decompose()
+         #Deletion of the <lb/> inserted before a <pb/>
+
+with open(sys.argv[1].replace(".xml", "_changed.xml"),"w") as file_out:
+    print("writing to "+sys.argv[1].replace(".xml", "_changed.xml"))
+    file_out.write(str(soup))
