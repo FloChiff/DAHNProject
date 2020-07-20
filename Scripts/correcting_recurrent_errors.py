4e40f5a6f765797c562664639f10c3fb61276ca8
FloChiff <floriane.chiffoleau@hotmail.fr>
2020-07-20 16:24:16 +0200

    Modifications on the 'correcting_recurrent_errors' script

--- Scripts/correcting_recurrent_errors.py
+++ Scripts/correcting_recurrent_errors.py
@@ -8,29 +8,33 @@
 - output: XML file
 """
 
+import os
 import sys
 from bs4 import BeautifulSoup
 
-with open(sys.argv[1], 'r') as xml_file:
-    print("reading from "+sys.argv[1])
-    tree = xml_file.read()
-tree = tree.replace('<p rend="indent"><closer>', '<closer>')
-tree = tree.replace('<p rend="indent"><salute rend="indent">', '<salute rend="indent">')
-#Deletion of the extra tags made because of the "text_tagging" script
 
-soup = BeautifulSoup(tree, 'xml')
+for root, dirs, files in os.walk(sys.argv[1]):
+    for filename in files:
+        with open(sys.argv[1] + filename, 'r') as xml_file:
+            print("reading from "+sys.argv[1] + filename)
+            tree = xml_file.read()
+        tree = tree.replace('<p rend="indent"><closer>', '<closer>')
+        tree = tree.replace('<p rend="indent"><salute rend="indent">', '<salute rend="indent">')
+        #Deletion of the extra tags made because of the "text_tagging" script
 
-n = 1
-for pb in soup.find_all("pb"):
-    pb.attrs["n"] = n
-    n += 1
-    #Filling the @n attribute by numbering the pages
+        soup = BeautifulSoup(tree, 'xml')
 
-    for lb in soup.find_all("lb"):
-        if pb.find_previous_sibling() == lb:
-            lb.decompose()
-         #Deletion of the <lb/> inserted before a <pb/>
+        n = 1
+        for pb in soup.find_all("pb"):
+            pb.attrs["n"] = n
+            n += 1
+            #Filling the @n attribute by numbering the pages
 
-with open(sys.argv[1].replace(".xml", "_changed.xml"),"w") as file_out:
-    print("writing to "+sys.argv[1].replace(".xml", "_changed.xml"))
-    file_out.write(str(soup))
+            for lb in soup.find_all("lb"):
+                if pb.find_previous_sibling() == lb:
+                    lb.decompose()
+                 #Deletion of the <lb/> inserted before a <pb/>
+
+        with open(sys.argv[1] + filename.replace(".xml", "_changed.xml"),"w") as file_out:
+            print("writing to "+sys.argv[1] + filename.replace(".xml", "_changed.xml"))
+            file_out.write(str(soup))
