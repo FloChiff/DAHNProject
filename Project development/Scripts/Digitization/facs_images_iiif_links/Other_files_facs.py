502fe4dacef47bcbadf877d18d97fd447f0af41a
FloChiff <floriane.chiffoleau@hotmail.fr>
2021-07-19 13:08:41 +0200

    Added new facs ID for the BI files

---
+++ Project development/Scripts/Digitization/facs_images_iiif_links/Other_files_facs.py
@@ -0,0 +1,8 @@
+Herz_an_Bekker = {"demo-46-cut.jpg": "10.34847/nkl.61bafea1/dfc38caf0e786c65fe399d27802494904d0a1d9d",
+"demo-45-cut.jpg": "10.34847/nkl.61bafea1/625185befe3a6c3c92c3c1444434b48eac364653"}
+
+Brief010altensteinanfakultaet = {"00000343.jpg": "10.34847/nkl.ca3b1x8b/39a4314c5d77796cc5d4f0ead81b1cd21062eaf9"}
+
+Brief013nicoluviusanfakultaet = {"00000349.jpg": "10.34847/nkl.34b2m5yb/181a81acadfc58a1ebd6c04bb0cd2227cfd3f29e"}
+
+Brief014solgeranfakultaet = {"00000350.jpg": "10.34847/nkl.90bfyu31/f0fb552e33fcdcd7067e5373f79256c06c862bfa"}
